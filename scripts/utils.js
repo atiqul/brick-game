94a836e586e57cd3f8bce193f8ddacc49584fcfd
a_alam <atiqul.alam.rocky@gmail.com>
2016-12-10 22:13:28 +0600

    Add left and right move through the canvas.
    Signed-off-by: a_alam <atiqul.alam.rocky@gmail.com>

--- scripts/utils.js
+++ scripts/utils.js
@@ -2,11 +2,15 @@ var addY=false;
 var addX=true;
 function move(e){	
 	e = e || window.event;		
-	if(e.keyCode == '39' && x+w<width){		
-		x += 10;
+	console.log(x+(baseColumn*20)+objWidth)
+	console.log(e.keyCode)
+	var endX = x+(baseColumn*20)+objWidth;
+	var startX = x+(baseColumn*20);
+	if(e.keyCode == '39' && endX<width){		
+		x += 20;
 	}
-	if(e.keyCode == '37' && x>0){		
-		x = x-10
+	if(e.keyCode == '37' && startX>0){		
+		x = x-20
 	}
 	if(e.keyCode == '40' && y+h < height-50){		
 		y = y+50;
