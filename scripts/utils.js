e330ce6ad7157ee5c8ed90639d99f5f377da3c46
a_alam <atiqul.alam.rocky@gmail.com>
2016-12-11 01:05:28 +0600

    Add orientation functionality for every brick object. When click up arrow it will rotate 90 degree clockwise direction.
    Signed-off-by: a_alam <atiqul.alam.rocky@gmail.com>

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,8 +1,7 @@
 var addY=false;
 var addX=true;
 function move(e){	
-	e = e || window.event;		
-	console.log(x+(baseColumn*20)+objWidth)
+	e = e || window.event;			
 	console.log(e.keyCode)
 	var endX = x+(baseColumn*20)+objWidth;
 	var startX = x+(baseColumn*20);
@@ -12,8 +11,12 @@ function move(e){
 	if(e.keyCode == '37' && startX>0){		
 		x = x-20
 	}
-	if(e.keyCode == '40' && y+h < height-50){		
-		y = y+50;
+	if(e.keyCode == '40' && y+h < height-50){
+	console.log("called")		
+		y = y+(2*blockHeight);
+	}
+	if(e.keyCode == '38' && y+h < height-50){						
+		obj[0].orientation();		
 	}
 
 }
